ad3da1bea727aa851d8d1b0571e5d58579d77be4
AlecJY <ae40515@yahoo.com.tw>
2022-04-06 14:07:32 +0800

    Add quality enums

---
+++ src/main/java/com/alebit/sget/playlist/Quality.java
@@ -0,0 +1,17 @@
+package com.alebit.sget.playlist;
+
+public enum Quality {
+    NONE(0),
+    MIN(-1),
+    MAX(-2);
+
+    private int value;
+
+    Quality(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
